e1b8b08288874fec8a7a24e3a84788bb612e1c12
Martin Finkel <finkel2804@gmail.com>
2020-08-04 11:16:30 +0200

    add initial divlc options

--- Program.cs
+++ Program.cs
@@ -17,23 +17,23 @@ namespace divlc
         const string libvlc3 = "vlc-3.0";
         const string LibVLC4URL = "https://github.com/videolan/vlc";
         const string LibVLC3URL = "https://github.com/videolan/vlc-3.0";
+        static string vlc4Dir = Path.Combine(Directory.GetCurrentDirectory(), libvlc4);
+        static string vlc3Dir = Path.Combine(Directory.GetCurrentDirectory(), libvlc3);
+
         public class Options
         {
-            //[Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-            //public bool Verbose { get; set; } = true;
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; } = true;
 
-            //[Option('t', "torrent", Required = false, HelpText = "The torrent link to download and play")]
-            //public string Torrent { get; set; } = "http://www.publicdomaintorrents.com/bt/btdownload.php?type=torrent&file=Charlie_Chaplin_Mabels_Strange_Predicament.avi.torrent";
+            [Option("libvlc4", Required = false, HelpText = "The hash of the libvlc 4 version to compare.")]
+            public string LibVLC4Hash { get; set; } = "6099ac613e9b99116d94371845f88808bfe8e626";
 
-            //// TODO: If multiple chromecast on the network, allow selecting it interactively via the CLI
-            //[Option('c', "cast", Required = false, HelpText = "Cast to the chromecast")]
-            //public bool Chromecast { get; set; }
+            [Option("libvlc3", Required = false
+                , HelpText = "The hash of the libvlc 3 version to compare.")]
+            public string LibVLC3Hash { get; set; } = "3f633b2777a1ce2e41e9869cb49992960294369b";
 
-            //[Option('s', "save", Required = false, HelpText = "Whether to save the media file. Defaults to true.")]
-            //public bool Save { get; set; } = true;
-
-            //[Option('p', "path", Required = false, HelpText = "Set the path where to save the media file.")]
-            //public string Path { get; set; } = Environment.CurrentDirectory;
+            [Option("noclone", Required = true, HelpText = "Do not clone, use the provided local git repositories.")]
+            public bool NoClone { get; set; }
         }
 
         static async Task Main(string[] args)
@@ -50,33 +50,41 @@ namespace divlc
 
         private static async Task RunOptions(Options cliOptions)
         {
-            //TODO: check if clones already exist and if we want to use it.
-            //TODO: check and log clone progress
-            var vlc4Dir = Path.Combine(Directory.GetCurrentDirectory(), libvlc4);
-            var vlc3 = Path.Combine(Directory.GetCurrentDirectory(), libvlc3);
-            var clone = false;
+            SetupGitRepositories(cliOptions);
 
-            if (clone)
-            {
-                if (Directory.Exists(vlc4Dir))
-                    Directory.Delete(vlc4Dir, true);
-
-                var cloneOptions = new CloneOptions
-                {
-                    OnTransferProgress = progress => { WriteLine($"{progress.ReceivedBytes}/{progress.TotalObjects}"); return true; }
-                };
-
-                var vlc4Repo = Repository.Clone("https://code.videolan.org/videolan/vlc.git", vlc4Dir, cloneOptions);
-                using var vlc4 = new Repository(vlc4Repo);
-                // TODO: checkout specific commit
-            }
-
-
-            var exampleFile = Path.Combine(vlc4Dir, include, vlc, libvlcHeader);
+            var exampleFile = Path.Combine(vlc3Dir, include, vlc, libvlcHeader);
+            var exampleFile2 = Path.Combine(vlc4Dir, include, vlc, libvlcHeader);
             var r = CppParser.ParseFile(exampleFile);
+            var r2 = CppParser.ParseFile(exampleFile2);
+
 
 
 
         }
+
+        static void SetupGitRepositories(Options cliOptions)
+        {
+            //TODO: check if clones already exist and if we want to use it.
+            //TODO: check and log clone progress
+            if (!cliOptions.NoClone)
+            {
+                if (Directory.Exists(vlc4Dir))
+                    Directory.Delete(vlc4Dir, true);
+                if (Directory.Exists(vlc3Dir))
+                    Directory.Delete(vlc3Dir, true);
+
+               /* var cloneOptions = new CloneOptions
+                {
+                    OnTransferProgress = progress => { WriteLine($"{progress.ReceivedBytes}/{progress.TotalObjects}"); return true; }
+                };*/
+
+                var vlc4Repo = Repository.Clone(LibVLC4URL, vlc4Dir);
+                var vlc3Repo = Repository.Clone(LibVLC3URL, vlc3Dir);
+                using var vlc4 = new Repository(vlc4Repo);
+                // TODO: checkout specific commit
+            }
+        }
+
+
     }
 }
